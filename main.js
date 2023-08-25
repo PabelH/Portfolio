b7f555ba049e212b5daa492e862f67c8d9c97ed4
Pabel Hernandez <pabel.hernandez@live.com>
2023-08-25 13:13:30 -0600

    update

--- main.js
+++ main.js
@@ -7,3 +7,19 @@ themeSwitch.addEventListener('change', () => {
         document.documentElement.setAttribute('data-bs-theme', 'dark');
     }
 });
+
+
+const showMoreButton = document.getElementById('show-more-button');
+const hiddenItems = document.getElementById('hidden-items');
+let isHidden = true;
+
+showMoreButton.addEventListener('click', () => {
+  if (isHidden) {
+    hiddenItems.style.display = 'block';
+    showMoreButton.textContent = 'Show -';
+  } else {
+    hiddenItems.style.display = 'none';
+    showMoreButton.textContent = 'Show +';
+  }
+  isHidden = !isHidden;
+});
